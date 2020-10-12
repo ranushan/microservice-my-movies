143586062e62a225fe2616d055520c7a4a9b934c
Ranushan <ranusha.n@hotmail.fr>
2020-10-12 14:47:15 +0200

    [FEATURE][BACK][RECOMMENDATION][UPDATED] microservice movie recommendation

--- datamining-movie-service/python.sh
+++ datamining-movie-service/python.sh
@@ -1,5 +1,4 @@
 sudo apt-get install python3-pip -y
-wget https://github.com/ranushan/microservice-my-movies/raw/master/datamining-movie-service/movie_dataset.csv
 pip3 install sklearn
 pip3 install pandas
 pip3 install bottle
